a2462dbb7cf19af9581bae8fa18859c063211cc3
Dhana <dhanaram367@gmail.com>
2024-07-31 04:27:57 +0530

    Project Created Successfully

---
+++ src/main/java/com/idiots/IdiotsShop/IdiotsShopApplication.java
@@ -0,0 +1,13 @@
+package com.idiots.IdiotsShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdiotsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdiotsShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idiots/IdiotsShop/IdiotsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idiots.IdiotsShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdiotsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
